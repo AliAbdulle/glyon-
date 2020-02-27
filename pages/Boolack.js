4196c3634da00c8af4809eefb461aff01f41ea5f
AliAbdulle <alidhobey@gmail.com>
2020-02-26 21:45:48 -0600

    Added constructor

--- pages/Boolack.js
+++ pages/Boolack.js
@@ -2,5 +2,10 @@ import React from 'react';
 import '/index.css';
 
 class Boolack extends React.Component {
-    
+    constructor() {
+        state = {
+            name: 'Ali',
+            date : ''
+        }
+    }
 }
